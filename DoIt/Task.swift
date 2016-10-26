40477b98d1593ba01a5db15d0ec96ce6d9d36033
Kyle Clapman <kyle.clapman@gmail.com>
2016-10-25 17:34:30 -0700

    Added 'Back' button

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Kyle Clapman on 10/25/16.
+//  Copyright © 2016 Kyle Clapman. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    var name = ""
+    var important = false
+}
